a0d0545615ba721af6777af2a623bd4b9a1c7de8
Tom LanFranca <ittechtommy@gmail.com>
2018-06-14 10:39:26 -0500

    Completed Conditionals and Lists Exercise

---
+++ Conditionals and lists/conditionalsAndLists.js
@@ -0,0 +1,17 @@
+
+new Vue({
+  el: "#exercises",
+  data: {
+    show: true,
+    people: ["Tom", "Pete", "Katy", "Ade"],
+    books:{
+      title: "Lord of the Rings", 
+      author: "J.R.R Tolkien", 
+      books: 3
+    },
+    test: {
+      name: "TESTOBJECT",
+      data: [1.67, 1.33, 0.98, 2.21]
+    }
+  }
+});

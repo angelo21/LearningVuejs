d57fd67ac27d68d8b4c048fc70f1d7fe2d6fc514
Tom LanFranca <ittechtommy@gmail.com>
2018-06-20 05:10:15 -0500

    Basic funtionality completed. Adding quotes and deleting quotes done

--- Quote_Project/src/App.vue
+++ Quote_Project/src/App.vue
@@ -1,13 +1,41 @@
 <template>
-    <div class="container">
-    
+  <div class="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it</div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data
+  import appQuote from "./components/Quote.vue";
+  import appGrid from "./components/QuoteGrid.vue";
+  import appNewQuote from "./components/NewQuote.vue";
+
+  export default {
+    data() {
+      return {
+      quotes: [],
+      maxQuotes: 10,
+      }
+    },
+    methods: {
+      newQuote(event) {
+        this.quotes.push(event)
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1);
+      }
+    },
+    components: {
+      appQuote,
+      appGrid,
+      appNewQuote
     }
+  }
 </script>
 
 <style>
---
+++ Quote_Project/src/components/QuoteGrid.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div class="row">
+    <app-quote  
+      v-for="(item, index) of quotes" 
+      :key="item"
+      @click.native="deleteQuote(index)">
+      {{ item }}
+    </app-quote>
+  </div>
+</template>
+
+<script>
+import appQuote from "./Quote.vue";
+
+  export default {
+  props: ["quotes"],
+  components: {
+      appQuote
+    },
+    methods: {
+      deleteQuote(index) {
+        this.$emit("quoteDeleted", index)
+      }
+    }
+  }
+</script>
+
+<style>
+</style>

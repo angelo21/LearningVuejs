f799a74e6447c674dd631374fc2b4a2d42a07bfc
Tom LanFranca <ittechtommy@gmail.com>
2018-07-02 10:42:17 -0500

    Basic functionality completed

--- word-translator/src/App.vue
+++ word-translator/src/App.vue
@@ -5,19 +5,31 @@
     <app-translate-form   
       v-on:formSubmit="translateText">
     </app-translate-form>
+    <app-translate-output   
+      :translatedText="translatedText">
+    </app-translate-output>
   </div>
 </template>
 
 <script>
 import AppTranslateForm from "./components/TranslateForm";
+import AppTranslateOutput from "./components/TranslateOutput";
 
 export default {
+  data() {
+    name: "App"
+    return {
+      translatedText: "",
+    }
+  },
   components: {
     AppTranslateForm,
+    AppTranslateOutput
   },
   methods: {
-    translateText(value) {
-      console.log(value)
+    translateText(value, lang) {
+      this.$http.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180702T150202Z.6fc71af78012a72d.defa3e73ef20326936745e20e39c6490fd2f8a9e&lang=${lang}&text= + ${value}`)
+      .then((res) => this.translatedText = res.body.text[0])
     }
   }
 }
--- word-translator/src/components/TranslateForm.vue
+++ word-translator/src/components/TranslateForm.vue
@@ -6,6 +6,11 @@
         type="text" 
         v-model="textToTranslate" 
         placeholder="Enter a Word">
+        <select v-model="language">
+          <option value="es">Spanish</option>
+          <option value="ru">Russian</option>
+          <option value="fr">French</option>
+        </select>
         <input type="submit" value="Translate">
     </form>
   </div>
@@ -14,14 +19,16 @@
 <script>
 
 export default {
+  name: "Translate-form",
   data() {
     return {
       textToTranslate: "",
+      language: "es",
     }
   },
   methods: {
     formSubmit() {
-      this.$emit("formSubmit", this.textToTranslate)
+      this.$emit("formSubmit", this.textToTranslate, this.language)
     }
   }
 }

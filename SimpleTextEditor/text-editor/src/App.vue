da075434844fc0d0e23ad5cb0a5fc77565944cc1
Tom LanFranca <ittechtommy@gmail.com>
2018-07-02 08:31:05 -0500

    Completed layout. Beginning data binding between components

--- SimpleTextEditor/text-editor/src/App.vue
+++ SimpleTextEditor/text-editor/src/App.vue
@@ -1,20 +1,58 @@
 <template>
-  <div id="app">
-
+  <div id="app" class="main" @click="popup">
+    <app-popup 
+      v-if="show" 
+      :text.sync="textContent">
+    </app-popup>
+    
+    <div class="main__contain">
+      <p class="main__heading">Hello World</p>
+      <p class="main__subheading">Click Anywhere</p>
+    </div>
   </div>
 </template>
 
 <script>
+import AppPopup from "./components/Popup";
+
 export default {
   name: 'app',
   data () {
     return {
-      
+      show: false,
+      textContent: "",
     }
+  },
+  methods: {
+    popup() {
+      this.show = !this.show;
+    }
+  },
+  components: {
+    AppPopup,
   }
 }
 </script>
 
 <style lang="scss">
+  .main {
+    cursor: default;
 
+    &__contain {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      color: #6D8088;
+      font-size: 25px;
+      font-weight: bold;
+      font-family: sans-serif;
+      text-align: center;
+    }
+
+    &__subheading {
+      font-size: 12px;
+    }
+  }
+  
 </style>
---
+++ SimpleTextEditor/text-editor/src/components/Popup.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>
+    <input type="text" class="popup__input">
+  </div>
+</template>
+
+<style lang="scss">
+  .popup__input {
+    position: absolute;
+    top: 35%;
+    left: 50%;
+    transform: translateX(-50%);
+    border: 7px solid #5B9AB6;
+    border-radius: 6px;
+    padding: 10px;
+    width: 300px;
+  }
+</style>
+

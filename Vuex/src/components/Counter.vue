e952973300fbc100fead624229cd9b61fbd2c93f
Tom LanFranca <ittechtommy@gmail.com>
2018-07-03 10:42:32 -0500

    Learning vuex getters, mutations, and actions

--- Vuex/src/components/Counter.vue
+++ Vuex/src/components/Counter.vue
@@ -1,19 +1,18 @@
 <template>
     <div>
-        <button class="btn btn-primary" @click="increment">Increment</button>
-        <button class="btn btn-primary" @click="decrement">Decrement</button>
+        <button class="btn btn-primary" @click="increment(100)">Increment</button>
+        <button class="btn btn-primary" @click="decrement(50)">Decrement</button>
     </div>
 </template>
 
 <script>
+    import { mapMutations } from "vuex";
     export default {
         methods: {
-            increment() {
-                this.$emit('updated', 1);
-            },
-            decrement() {
-                this.$emit('updated', -1);
-            }
+            ...mapMutations([
+                "increment",
+                "decrement"
+            ])
         }
     }
 </script>

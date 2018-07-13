a10b81b56d66b48a0ed5aa0cecab0518e2d57d30
Tom LanFranca <ittechtommy@gmail.com>
2018-07-13 10:52:51 -0500

    Added router and basic routes

---
+++ Router/src/components/Header.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <ul class="nav nav-pills">
+    <router-link 
+      to="/"
+      tag="li"
+      exact
+      active-class="active"
+      ><a>Home</a>
+    </router-link>
+    <router-link 
+      to="/user"
+      tag="li"
+      active-class="active"><a>User</a>
+    </router-link>
+  </ul>
+</template>
+
+
+
